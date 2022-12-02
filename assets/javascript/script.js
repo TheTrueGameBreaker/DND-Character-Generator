b41d47127cc6a8f3c59ecb43e944bed660121ec0
Maddie Gillis <madelinegillis@outlook.com>
2022-12-01 19:12:40 -0800

    added query selectors/variables for html to JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,20 @@
+//HTML selectors
+var newCharacterBtn = document.getElementsByClassName('new-character');
+var savedBtn = document.getElementsByClassName('saved');
+var containerEl = document.getElementsByClassName('container');
+var imgEl = document.getElementsByClassName('placeholder-Image');
+var raceEl = document.getElementById('race');
+var classEl = document.getElementById('class')
+var alignmentEl = document.getElementById('alignment')
+var statsContainerEl = document.getElementsByClassName('container-Stats');
+var strengthEl = document.getElementById('strength');
+var dexterityEl = document.getElementById('dexterity');
+var constitutionEl = document.getElementById('constitution');
+var intelligenceEl = document.getElementById('intelligence');
+var wisdomEl = document.getElementById('wisdom');
+var charismaEl = document.getElementById('charisma');
+
+//API selectors
+var raceUrl = `https://www.dnd5eapi.co`
+
 console.log('linked')
