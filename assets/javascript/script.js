21645878a0247b3e5aa66dc13a05cf0cc426e386
Davis N Genthner <115984743+TheTrueGameBreaker@users.noreply.github.com>
2022-12-05 20:30:05 -0700

    Add files via upload

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,51 +1,61 @@
 //HTML selectors
-var newCharacterBtn = document.getElementsByClassName('new-character');
-var savedBtn = document.getElementsByClassName('saved');
-var containerEl = document.getElementsByClassName('container');
-var imgEl = document.getElementsByClassName('placeholder-Image');
-var raceEl = document.getElementById('race');
-var classEl = document.getElementById('class')
-var alignmentEl = document.getElementById('alignment')
-var statsContainerEl = document.getElementsByClassName('container-Stats');
-var strengthEl = document.getElementById('strength');
-var dexterityEl = document.getElementById('dexterity');
-var constitutionEl = document.getElementById('constitution');
-var intelligenceEl = document.getElementById('intelligence');
-var wisdomEl = document.getElementById('wisdom');
-var charismaEl = document.getElementById('charisma');
-var statsEl = document.getElementsByClassName('stats');
-var rollBtn = document.getElementById('rollBtn');
-var statId = document.getElementById('stat');
+const newCharacterBtn = document.getElementsByClassName('new-character');
+const savedBtn = document.getElementsByClassName('saved');
+const containerEl = document.getElementsByClassName('container');
+const imgEl = document.getElementsByClassName('placeholder-Image');
+const raceEl = document.getElementById('race');
+const classEl = document.getElementById('class')
+const alignmentEl = document.getElementById('alignment')
+const statsContainerEl = document.getElementsByClassName('container-Stats');
+const strengthEl = document.getElementById('strength');
+const dexterityEl = document.getElementById('dexterity');
+const constitutionEl = document.getElementById('constitution');
+const intelligenceEl = document.getElementById('intelligence');
+const wisdomEl = document.getElementById('wisdom');
+const charismaEl = document.getElementById('charisma');
+const statsEl = document.getElementsByClassName('stats');
+const genBtn = document.getElementById('Gen-Btn');
+const statId = document.getElementById('stat');
 
 
 //DND5E API selectors
-var apiServerUrl = `https://www.dnd5eapi.co`;
-var apiClassUrl = `/api/classes/`;
-var apiRaceUrl = `/api/races/`;
-var apiAlignmentUrl = `/api/alignments/`;
+const apiServerUrl = `https://www.dnd5eapi.co`;
+const apiClassUrl = `/api/classes/`;
+const apiRaceUrl = `/api/races/`;
+const apiAlignmentUrl = `/api/alignments/`;
 
 //Dog API selectors
-var dogApiUrl = `https://dog.ceo/api/breeds/image/random`
+const dogApiUrl = `https://dog.ceo/api/breeds/image/random`
 
 
 
-
+genBtn.addEventListener('click', statGen)
 
 
 //random stats function
-var statArray = [];
-for (i = 0; i < 6; i++) {
-  var statNumber = Math.floor(Math.random() * 16) + 4
-  statArray.push(statNumber); 
-}
+function statGen() {
+  var statArray = [];
+  for (i = 0; i < 6; i++) {
+    var statNumber = Math.floor(Math.random() * 16) + 4
+    statArray.push(statNumber); 
+  }
 
-console.log(statArray);
-strengthEl.append(statArray[0]);
-dexterityEl.append(statArray[1]);
-constitutionEl.append(statArray[2]);
-intelligenceEl.append(statArray[3]);
-wisdomEl.append(statArray[4]);
-charismaEl.append(statArray[5]);
+  strengthEl.innerHTML = `<h1 id="strength"> Strength: </h1>`
+  dexterityEl.innerHTML = `<h1 id="dexterity"> Dexterity: </h1>`
+  constitutionEl.innerHTML = `<h1 id="constitution"> Constitution: </h1>`
+  intelligenceEl.innerHTML = `<h1 id="intelligence"> Intelligence: </h1>`
+  wisdomEl.innerHTML = `<h1 id="wisdom"> Wisdom: </h1>`
+  charismaEl.innerHTML = `<h1 id="charisma"> Charisma: </h1>`
+
+
+  console.log(statArray);
+  strengthEl.append(statArray[0]);
+  dexterityEl.append(statArray[1]);
+  constitutionEl.append(statArray[2]);
+  intelligenceEl.append(statArray[3]);
+  wisdomEl.append(statArray[4]);
+  charismaEl.append(statArray[5]);
+}
 
 
 
